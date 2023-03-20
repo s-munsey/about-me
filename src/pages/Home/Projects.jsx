59ef2d16d72c3cf5bfe21def804bc52185fda166
Shaun Munsey <s_munsey@live.com>
2023-03-20 15:53:05 +0000

    home page wrap up

--- src/pages/Home/Projects.jsx
+++ src/pages/Home/Projects.jsx
@@ -7,55 +7,62 @@ const Projects = () => {
       <section id="projects">
         <h2>Projects I'm proud of:</h2>
         <article>
-          <h4>Current Project</h4>
-          <h3>Bookshelf</h3>
-          <p className="blackbox">
-            I love to read, so I'm working on an application, using the open
-            library api, that can track the books you have, your reading
-            progress, and how many books you've read each year. You can follow
-            its progress <a href="../../assets/">here</a>.
-          </p>
-          <h4>Technologies used include:</h4>
-          <ul>
-            <li>React</li>
-            <li>Node</li>
-            <li>TypeScript</li>
-          </ul>
+          <div className="text">
+            <h4>Current Project</h4>
+            <h3>Bookshelf</h3>
+            <p className="blackbox">
+              I love to read, so I'm working on an application, using the open
+              library api, that can track the books you have, your reading
+              progress, and how many books you've read each year. You can follow
+              its progress <a href="../../assets/">here</a>.
+            </p>
+            <h4>Technologies used include:</h4>
+            <ul>
+              <li>React</li>
+              <li>Node</li>
+              <li>TypeScript</li>
+            </ul>
+          </div>
           <img src={require("./../../assets/wip.png")} />
         </article>
-        <article>
-          <h4>App I published</h4>
-          <h3>Pomodoro App</h3>
-          <p>
-            I've always got some project on the go, this is the first one I
-            actually published, it helped me focused during many a project, and
-            if you're fortunate enough to own a pebble (they're sadly
-            discontinued) can download the app{" "}
-            <a href="https://apps.rebble.io/en_US/application/573a3ae54264000fe3000022?query=reubeniv&section=watchapps">
-              here
-            </a>
-            .
-          </p>
-          <h4>Technologies used include:</h4>
-          <ul>
-            <li>Pebble SDK</li>
-            <li>C</li>
-          </ul>
+        <article className="reverse">
+          <div className="text">
+            <h4>App I published</h4>
+            <h3>Pomodoro App</h3>
+            <p className="blackbox">
+              I've always got some project on the go, this is the first one I
+              actually published, it helped me focused during many a project,
+              and if you're fortunate enough to own a pebble (they're sadly
+              discontinued) can download the app{" "}
+              <a href="https://apps.rebble.io/en_US/application/573a3ae54264000fe3000022?query=reubeniv&section=watchapps">
+                here
+              </a>
+              .
+            </p>
+            <h4>Technologies used include:</h4>
+            <ul>
+              <li>Pebble SDK</li>
+              <li>C</li>
+            </ul>
+          </div>
           <img src={require("./../../assets/pomo_previewsq.png")} />
         </article>
         <article>
-          <h4>University Project</h4>
-          <h3>AI Music Classifier</h3>
-          <p>
-            Another passion of mine is music, and using the million song dataset
-            I built an artificial neural net that can compare, for example the
-            loudness, bpm, etc, and (somewhat) accurately classify the genre
-            <a href="">here</a>.
-          </p>
-          <h4>Technologies used include:</h4>
-          <ul>
-            <li>Python</li>
-          </ul>
+          <div className="text">
+            <h4>University Project</h4>
+            <h3>AI Music Classifier</h3>
+            <p className="blackbox">
+              Another passion of mine is music, and using the million song
+              dataset I built an artificial neural net that can compare, for
+              example the loudness, bpm, etc, and (somewhat) accurately classify
+              the genre
+              <a href="">here</a>.
+            </p>
+            <h4>Technologies used include:</h4>
+            <ul>
+              <li>Python</li>
+            </ul>
+          </div>
           <img src={require("./../../assets/annsq.jpg")} />
         </article>
       </section>
