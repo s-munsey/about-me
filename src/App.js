51ba0a560a9768d1b41f45a0a2806c419458abe4
Shaun Munsey <s_munsey@live.com>
2023-03-20 22:43:01 +0000

    remove browserRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home/Home";
